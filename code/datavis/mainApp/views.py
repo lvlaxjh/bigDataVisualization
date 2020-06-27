146203b92da8048c776530698207fde7eb5aae88
lvlaxjh <2876363140@qq.com>
2020-06-27 17:44:07 +0800

    完成后端框架

--- code/datavis/mainApp/views.py
+++ code/datavis/mainApp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.http import JsonResponse
+import os
+import json
 # Create your views here.
+
+
+def index(request):
+    response = render(request, 'index.html')
+    return response
+
+
+def returnData(request):
+    result = {}
+    return JsonResponse(json.dumps(result))
