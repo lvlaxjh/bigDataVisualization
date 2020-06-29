302f0b03366f10095a533f7c01be0a00ae8e9f58
lvlaxjh <2876363140@qq.com>
2020-06-29 12:13:45 +0800

    更新后端，新增json全部数据

--- code/datavis/mainApp/views.py
+++ code/datavis/mainApp/views.py
@@ -6,19 +6,74 @@ import json
 import demjson
 # 用于显示主界面
 
+# 用于在启动前将数据全部读入内存------------------------------------------------------------------------------------------------
+pwd = os.getcwd()
 print('---后端---')
 print('---读取数据---')
-xiaxingFile = open(
-    "/Users/lvlaxjh/code/code/bigDataVisualization/bigDataVisualization/code/datavis/mainApp/data/down.txt")
-lineNum = 0
-downData = {}
-for line in xiaxingFile:
-    print(line.replace('\n', '').split(','))
+allData = {}  # 存储全部数据
+# 存储上下行内容------------------------------------------------------------------------------------------------
+xiaxingFile1 = open(pwd+'/mainApp'+'/data'+'/beijing_xiaxing.txt')
+xiaxingFile2 = open(pwd+'/mainApp'+'/data'+'/dongguan_xiaxing.txt')
+xiaxingFile3 = open(pwd+'/mainApp'+'/data'+'/nanjing_xiaxing.txt')
+shangxingFile1 = open(pwd+'/mainApp'+'/data'+'/beijing_shangxing.txt')
+shangxingFile2 = open(pwd+'/mainApp'+'/data'+'/dongguan_shangxing.txt')
+shangxingFile3 = open(pwd+'/mainApp'+'/data'+'/nanjing_shangxing.txt')
+fileList = []  # 上下行文件列表
+fileList.append(xiaxingFile1)
+fileList.append(xiaxingFile2)
+fileList.append(xiaxingFile3)
+fileList.append(shangxingFile1)
+fileList.append(shangxingFile2)
+fileList.append(shangxingFile3)
+allData["shangxing"] = {}
+allData["xiaxing"] = {}
+siteName = []  # 存储地点名称
+for file in fileList:
+    fileName = file.name.split('/')[-1][:-4]
+    fileName1 = fileName.split('_')[0]  # beijing
+    fileName2 = fileName.split('_')[1]  # xiaxing
+    lineNum = 0  # 记录文件行数
+    allData[fileName2][fileName1] = {}
+    for line in file:
+        oneLine = line.replace('\n', '').split(',')
+        if lineNum == 0:
+            siteName = oneLine
+        else:
+            oneDict = allData[fileName2][fileName1][oneLine[0]] = {}
+            for i in range(len(oneLine)):
+                if i != 0:
+                    oneDict[siteName[i]] = oneLine[i]
+        lineNum += 1
+# print(allData)
+for i in fileList:
+    i.close()
+# 存储链路内容------------------------------------------------------------------------------------------------
+lianluFile = open(pwd+'/mainApp'+'/data'+'/lianlu.txt')
+siteName = []  # 存储地点名称
+allData['lianlu'] = {}
+lineNum = 0  # 记录文件行数
+for line in lianluFile:
+    oneLine = line.replace('\n', '').split(',')
     if lineNum == 0:
-        # downData =
-        pass
+        siteName = oneLine
+    else:
+        oneDict = allData['lianlu'][oneLine[0]] = {}
+        oneDict['beijing'] = {}
+        oneDict['dongguan'] = {}
+        oneDict['nanjing'] = {}
+        oneDict['beijing']['dongguan'] = oneLine[1]
+        oneDict['beijing']['nanjing'] = oneLine[2]
+        oneDict['dongguan']['beijing'] = oneLine[3]
+        oneDict['dongguan']['nanjing'] = oneLine[4]
+        oneDict['nanjing']['beijing'] = oneLine[5]
+        oneDict['nanjing']['dongguan'] = oneLine[6]
     lineNum += 1
-
+lianluFile.close()
+# file = open('/Users/lvlaxjh/code/code/bigDataVisualization/bigDataVisualization/code/datavis/mainApp/test.json', 'w')
+# # print(allData)
+# file.write(json.dumps(allData))
+# file.close()
+# 存储任务内容------------------------------------------------------------------------------------------------
 
 
 def index(request):
@@ -33,5 +88,4 @@ def returnData(request):
     # 获取前端的请求
     if request.method == "POST":
         require = demjson.decode(request.body)
-        print(require)
     return JsonResponse(result)
